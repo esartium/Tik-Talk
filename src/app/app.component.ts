6ef159a6b3b6c7db705c01e492bd51a077bc49a7
Экерт Софья Андреевна <cr_ovr_web10@efko.ru>
2024-07-04 17:39:25 +0300

    Некоторые пояснения

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// Это декоратор (что-то вроде набора настроек для класса)
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: './app.component.html', // указываем, какой html-шаблон у этого компонента
+  styleUrl: './app.component.scss' // указываем, какой файл отвечает за стили этого компонента
 })
-export class AppComponent {
+export class AppComponent { // сам класс компонента
   title = 'tik-talk';
 }
