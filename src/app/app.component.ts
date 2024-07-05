9557042bf9cfac504f7abdaaee26fe15cf200145
Экерт Софья Андреевна <cr_ovr_web10@efko.ru>
2024-07-05 13:46:46 +0300

    сделали сайд-бар (пока пустой), который будет на всех страницах, кроме страницы авторизации (сделали с помощью роутинга (использования layout))

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { ProfileCardComponent } from './common-ui/profile-card/profile-card.comp
 import { inject } from '@angular/core';
 import { JsonPipe } from '@angular/common';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { LayoutComponent } from './common-ui/layout/layout.component';
 
 // Это декоратор (что-то вроде набора настроек для класса)
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProfileCardComponent, JsonPipe, SearchPageComponent],
+  imports: [RouterOutlet, ProfileCardComponent, JsonPipe, SearchPageComponent, LayoutComponent],
   templateUrl: './app.component.html', // указываем, какой html-шаблон у этого компонента
   styleUrl: './app.component.scss' // указываем, какой файл отвечает за стили этого компонента
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,19 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+import { LayoutComponent } from './common-ui/layout/layout.component';
 
 // Routes - это массив объектов, представляющих собой путь до страницы + компонент этой страницы
 export const routes: Routes = [
-    {path: '', component: SearchPageComponent},
-    {path: 'login', component: LoginPageComponent}
-    // пустой путь - это корень сайта
+
+    // для создания дочернего роутера:
+    {path: '', component: LayoutComponent, children: [
+        {path: '', component: SearchPageComponent},
+        {path: 'profile', component: ProfilePageComponent}
+    ]},
+    
+    {path: 'login', component: LoginPageComponent},
 ];
 
 // по умолчанию роуты работать не будут;
