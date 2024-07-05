339c254cbebca28a27267fc7334229c84668aab3
Экерт Софья Андреевна <cr_ovr_web10@efko.ru>
2024-07-05 19:25:29 +0300

    Получаем из формы SubmitEvent (событие)

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
 
+  // Делаем инстанс форм-группы
+  form = new FormGroup({
+    username: new FormControl(null),
+    password: new FormControl(null),
+  })
+
+  onSubmit(event: Event):void {
+    console.log(event)
+  }
 }
