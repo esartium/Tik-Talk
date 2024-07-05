b02568bd2e08a5635954671040fc9c28f8dd1881
Экерт Софья Андреевна <cr_ovr_web10@efko.ru>
2024-07-05 11:57:35 +0300

    Вывели старым способом в повторяющихся элементах поля ответа в цикле; в том числе картинки (использовали кастомный пайп для url картинок)

--- src/app/common-ui/profile-card/profile-card.component.ts
+++ src/app/common-ui/profile-card/profile-card.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Profile } from '../../data/interfaces/profile.interface';
+import { ImgUrlPipe } from '../../helpers/pipes/img-url.pipe';
 
 @Component({
   selector: 'app-profile-card', // название тега, который мы будем использовать, чтобы разместить этот компонент в родительском компоненте
   standalone: true,
-  imports: [],
+  imports: [ImgUrlPipe],
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.scss'
 })
 export class ProfileCardComponent {
+  // используем декоратор @Input
+  @Input() profile?: Profile;
+  // тут написано примерно это:
+  // Input ожидает отправленные ему данные; он получит их и сохранит в profile;
+  // Мы не знаем точно, придёт ли profile => ?;
+  // Если придёт, то будет типом данных Profile
 
+  // @Input() profile!: Profile; это бы означало: "Успокойся, ts, я точно уверен, что данные придут "
 }
