be7707de6b528624d41633e3d99493c61c53299a
Экерт Софья Андреевна <cr_ovr_web10@efko.ru>
2024-07-06 15:59:41 +0300

    Сохранили ответ сервера в переменную

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -12,6 +12,13 @@ export class AuthService {
 
   baseApiUrl: string = 'https://icherniakov.ru/yt-course/auth/';
 
+  access_token: string | null = null;
+  refresh_token: string | null = null;
+
+  get isAuth() { // геттер для проверки того, авторизован ли пользователь с таким токеном
+    return !!this.access_token;
+  }
+
   login(payload: {username: string, password: string}) {
     // В этом методе на бэке принимаются данные не в привычном json, а в x-www-form-urlencoded
     // Как перевести их в этот формат:
@@ -23,8 +30,10 @@ export class AuthService {
     return this.http.post<TokenResponse>(`${this.baseApiUrl}token`, fd) // пока мы не указали тут <TokenResponse>, VScode предлагал после res. обычные методы объекта (типа res - это просто какой-то объект, хз какой); после указания - предлагает поля ответа, которые мы прописывали в интерфейсе
       .pipe(
         tap(
-          res => console.log("access_token: ", res.access_token, "refresh_token: ", res.refresh_token)
+          res => {
+            this.access_token = res.access_token;
+            this.refresh_token = res.refresh_token;
+          })
         )
-      )
   }
 }
