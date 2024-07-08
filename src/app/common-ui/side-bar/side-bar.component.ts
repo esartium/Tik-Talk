17c231f27ad2435402e8e3abf86a2ffce7903b38
Экерт Софья Андреевна <cr_ovr_web10@efko.ru>
2024-07-08 22:23:40 +0300

    Делаем сайдбар; спрайт почему-то не выводится

--- src/app/common-ui/side-bar/side-bar.component.ts
+++ src/app/common-ui/side-bar/side-bar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SvgIconComponent } from '../svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [],
+  imports: [SvgIconComponent],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss'
 })
