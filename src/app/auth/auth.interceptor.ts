2d788c89f09d45594d6013277279338d0f74adfc
Экерт Софья Андреевна <cr_ovr_web10@efko.ru>
2024-07-08 15:11:25 +0300

    Интерсептор; рефреш токена

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -1,8 +1,11 @@
 // Раньше интерсепторы реализовывались на классах; сейчас - функции
 
-import { HttpInterceptorFn, HttpRequest } from "@angular/common/http";
+import { HttpHandler, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from "@angular/common/http";
 import { AuthService } from "./auth.service";
 import { inject } from "@angular/core";
+import { catchError, switchMap, throwError } from "rxjs";
+
+let isRefreshing = false; // чтобы не попасть в петлю, запрос на рефреш будет проходить через интерсептор не так, как обычный
 
 export const authTokenInterceptor: HttpInterceptorFn = (request, next) => {
     // параметры:
@@ -10,25 +13,63 @@ export const authTokenInterceptor: HttpInterceptorFn = (request, next) => {
     // next - функция, чтобы отпустить запрос после того, как что-то с ним сделали
 
     // В этом интерсепторе будем добавлять заголовок авторизации к каждому запросу (посмотреть можно в консоли -> network -> любой запрос fetch/xhr -> headers)
-
     const access_token = inject(AuthService).access_token;
-
+    const authService = inject(AuthService);
+    
     if (!access_token) {
         return next(request);
     }
-    
-    request = request.clone(
+
+    if(isRefreshing) {
+        return refreshAndProceed(authService, request, next);
+    }
+
+    return next(addToken(request, access_token)).pipe(
+        catchError(err => {
+            if (err.status === 403) { // ошибка 403 у этого бэка означает, что токен недействителен (сдох)
+                return refreshAndProceed(authService, request, next) // возвращаем новый стрим, который сделаем сами; мы вынесли его в отдельную функцию
+            }
+
+            return throwError(err) // если ошибка не про токен, то просто возвращаем стрим с этой ошибкой
+        })
+    );
+
+}
+
+const refreshAndProceed = (authService: AuthService, request: HttpRequest<any>, next: HttpHandlerFn) => {
+    if (!isRefreshing) {
+        isRefreshing = true;
+
+        return authService.refreshAuthToken().pipe(
+            switchMap( // этот пайп меняет один стрим на другой
+                // в данном случае:
+                // мы делали какой-то запрос, его перехватил интерсептор.
+                // пока запрос находился в интерсепторе, выяснилось, что токен сдох.
+                // мы отправляем запрос на получение токена с бэка. этот запрос возвращает нам стрим с токеном.
+                // с этого стрима нам нужно будет переключиться на другой стрим - ещё раз отправить запрос, такой же, из которого мы изначально попали в интерсепто, но с новым токеном
+                (res) => { 
+                    isRefreshing = false;
+
+                    return next(addToken(request, res.access_token)) // тут токен можно было достать и не из res, а из authService
+                    // то есть здесь мы отпустили запрос - сделали то, что надо было сделать с изначальным запросом, но теперь с новым токеном
+                } 
+            )
+        )
+    }
+
+    return next(addToken(request, authService.access_token))
+
+}
+
+const addToken = (request: HttpRequest<any>, access_token: string | null) => {
+    return request = request.clone(
         {
             setHeaders: {
                 Authorization: `Bearer ${access_token}`
             }
         }
     )
-
-    return next(request);
 }
-
-
 
 
 
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { tap } from 'rxjs';
+import { catchError, tap, throwError } from 'rxjs';
 import { TokenResponse } from './auth.interface';
 import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -18,9 +19,12 @@ export class AuthService {
   access_token: string | null = null;
   refresh_token: string | null = null;
 
+  router = inject(Router);
+
   get isAuth() { // геттер для проверки того, авторизован ли пользователь с таким токеном
     if (!this.access_token) {
-      this.access_token = this.cookieServise.get('access_token')
+      this.access_token = this.cookieServise.get('access_token');
+      this.refresh_token = this.cookieServise.get('refresh_token')
     }
     return !!this.access_token;
   }
@@ -35,14 +39,37 @@ export class AuthService {
 
     return this.http.post<TokenResponse>(`${this.baseApiUrl}token`, fd) // пока мы не указали тут <TokenResponse>, VScode предлагал после res. обычные методы объекта (типа res - это просто какой-то объект, хз какой); после указания - предлагает поля ответа, которые мы прописывали в интерфейсе
       .pipe(
-        tap(
-          res => {
-            this.access_token = res.access_token;
-            this.refresh_token = res.refresh_token;
-
-            this.cookieServise.set('access_token', this.access_token);
-            this.cookieServise.set('refresh_token', this.refresh_token)
-          })
+        tap(res => this.saveTokens(res))
         )
   }
+
+  refreshAuthToken() {
+    return this.http.post<TokenResponse>(`${this.baseApiUrl}refresh`, {refresh_token: this.refresh_token})
+      .pipe(
+        tap(res => this.saveTokens(res)),
+        catchError(err => {
+          this.logout(); // делаем принудительный logout
+          return throwError(err); // выкидываем ошибку
+        })
+      )
+  }
+
+  logout() {
+    // вообще лучше сделать это через бэк, но тут мы просто сотрём куки
+    this.cookieServise.deleteAll(); 
+    // обнулим токены
+    this.access_token = null;
+    this.refresh_token = null;
+    // и перенаправим на страницу аворизации
+    this.router.navigate(['/login']);
+  }
+
+  saveTokens(res: TokenResponse) { // эта логика используется в двух местах, так что вынесли ее в отдельную функцию
+      this.access_token = res.access_token;
+      this.refresh_token = res.refresh_token;
+
+      this.cookieServise.set('access_token', this.access_token);
+      this.cookieServise.set('refresh_token', this.refresh_token)
+  }
+
 }
