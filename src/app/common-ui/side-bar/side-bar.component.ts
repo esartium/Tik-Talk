210071d0553f90084c09f9edf689b84f5400a186
Экерт Софья Андреевна <cr_ovr_web10@efko.ru>
2024-07-09 11:01:35 +0300

    вывели штуки в цикле старым способом (ngForOf); С НИМ И ИКОНКИ ВЫВЕЛИСЬ

--- src/app/common-ui/side-bar/side-bar.component.ts
+++ src/app/common-ui/side-bar/side-bar.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { SvgIconComponent } from '../svg-icon/svg-icon.component';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [SvgIconComponent],
+  imports: [SvgIconComponent, NgForOf],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss'
 })
 export class SideBarComponent {
-
+  menuItems = [
+    {
+      label: 'Моя страница',
+      icon: 'home',
+      link: '' // корень сайта
+    },
+    {
+      label: 'Чаты',
+      icon: 'chat',
+      link: 'chats' // корень сайта
+    },
+    {
+      label: 'Поиск',
+      icon: 'search',
+      link: 'search' // корень сайта
+    },
+  ]
 }
